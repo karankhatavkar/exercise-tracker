0decc8beedf3931b8fa2c21f8d0cc89291c2ef3e
karankhatavkar <karank1072@gmail.com>
2021-11-18 17:31:16 +0530

    Update exercises.js

--- backend/routes/exercises.js
+++ backend/routes/exercises.js
@@ -31,7 +31,7 @@ router.route('/:id').delete(((req, res) => {
 }));
 
 router.route('/update/:id').post(((req, res) => {
-    Exercise.findById((req.params.id)).then(exercise => {
+    Exercise.findById(req.params.id).then(exercise => {
         exercise.username = req.body.username;
         exercise.description = req.body.description;
         exercise.duration = req.body.duration;
