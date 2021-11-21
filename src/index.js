764dde53c25bb937d899424d3f8295b44ba007b9
karankhatavkar <karank1072@gmail.com>
2021-11-21 17:59:21 +0530

    exercise-form

--- src/index.js
+++ src/index.js
@@ -2,13 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
 // import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-    <Router history={Router} >
-        <Route path="/" component={App} />
-    </Router>,
+    <App />,
   document.getElementById('root')
 );
 
